142c6b20b1981fccdf41f8040697257d21b51bb0
arhamsc <shaik.arham@gmail.com>
2022-02-13 14:43:33 +0530

    Added title

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import './App.css';
+import DocumentMeta from 'react-document-meta';
 
 import NavBar from './components/NavBar/NavBar';
 import Banner from './components/Banner/Banner';
 import MovieRows from './components/MovieRows/MovieRows';
 
 function App() {
+  const meta = {
+    title: 'Netflix UI Clone',
+    description:
+      'Netflix clone capable of showing trailers and responsive layout.',
+    meta: {
+      charset: 'utf-8',
+      name: {
+        keywords: 'react,meta,document,html,tags',
+      },
+    },
+  };
 
   return (
     <div className="App">
+      <DocumentMeta {...meta} />
       <NavBar />
       <Banner />
       <MovieRows />
-      <div style={{
-        paddingBottom: "2rem"
-      }}/>
+      <div
+        style={{
+          paddingBottom: '2rem',
+        }}
+      />
     </div>
   );
 }
